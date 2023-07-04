4a84b7de5032cd5142630482df704294e0e75c29
PaulistaSO <nicolassoaresoliveira06@gmail.com>
2023-07-04 09:54:59 -0300

    sadsdasadsda

--- ecommerce/mercado/__init__.py
+++ ecommerce/mercado/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 db = SQLAlchemy()
 app = Flask(__name__)  
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
-app.config["SECRET_KEY"] = '7f813692d67182c0441d565e'
+app.config["SECRET_KEY"] = '21cd47a1be2c7a81c02665f3'
 db.init_app(app)
+bcrypt = Bcrypt(app)
 
 
 from mercado import routes
