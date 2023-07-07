c6a0b56e664be99f90098d2e845b8b22441fbd21
PaulistaSO <130756968+PaulistaSO@users.noreply.github.com>
2023-07-07 08:40:58 -0300

    parte IX finalizada

--- ecommerce/mercado/__init__.py
+++ ecommerce/mercado/__init__.py
@@ -11,6 +11,9 @@ app.config["SECRET_KEY"] = '21cd47a1be2c7a81c02665f3'
 db.init_app(app)
 bcrypt = Bcrypt(app)
 login_manager.init_app(app)
+login_manager.login_view = "page_login"
+login_manager.login_message = "Por favor, realize o login"
+login_manager.login_message_category = "info"
 
 
 from mercado import routes
